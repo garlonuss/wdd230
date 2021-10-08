a9070c4c2bd6e6b40d43c29374f9d289be6ebd65
garlonuss <garlonuss@hotmail.com>
2021-10-07 22:00:09 -0600

    update

--- activity-wk4/start-navigation/js/scripts.js
+++ activity-wk4/start-navigation/js/scripts.js
@@ -1,3 +1,4 @@
 function toggleMenu() {
-  console.log(document.getElementById("sally").classList)
+  console.log(document.getElementById("primaryNav").classList);
+  document.getElementById("primaryNav").classList.toggle("hide");
 }
