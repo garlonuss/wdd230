a4fafea1085be22a39148d30c5c263e304c3bfb4
garlonuss <garlonuss@hotmail.com>
2021-10-07 21:23:54 -0600

    updae

--- lesson3/js/myjavascript.js
+++ lesson3/js/myjavascript.js
@@ -1,2 +1,2 @@
-document.querySelector('#copyright').textContent = `(c) ${new Date().getFullYear()} | Ryan Peterson | Utah | BYU-I Online Learning`;
+document.querySelector('#copyright').textContent = `&#169; ${new Date().getFullYear()} | Ryan Peterson | Utah | BYU-I Online Learning`;
 document.querySelector('#lastupdated').textContent = `Last Updated: ${document.lastModified}`;
