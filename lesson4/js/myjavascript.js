43a49c4802ece0e7feabf2e3b0ab161d914ab5fd
garlonuss <garlonuss@hotmail.com>
2021-10-09 16:04:23 -0600

    update

--- lesson4/js/myjavascript.js
+++ lesson4/js/myjavascript.js
@@ -3,4 +3,23 @@ function toggleMenu() {
 }
 document.querySelector('#copyrightyear').textContent = new Date().getFullYear();
 document.querySelector('#lastupdated').textContent = `Last Updated: ${document.lastModified}`;
-document.querySelector('#todaysdate').textContent = `[Today: ${dateFormat(new Date(), 'pretty-a')}]`;
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+weekday[1] = "Sunday";
+weekday[2] = "Monday";
+weekday[3] = "Tuesday";
+weekday[4] = "Wednesday";
+weekday[5] = "Thursday";
+weekday[6] = "Friday";
+weekday[7] = "Saturday";
+document.querySelector('#todaysdate').textContent = `[Today: ${weekday[Date().getUTCDay()]}, ${month[Date().getMonth()]} ${Date().getDay()}, ${Date().getFullYear()}]`;
