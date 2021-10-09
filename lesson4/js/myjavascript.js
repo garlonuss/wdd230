767b9b70a9f0bc9720fb7cc84b6b9d7e7738156d
garlonuss <garlonuss@hotmail.com>
2021-10-09 09:16:24 -0600

    update

--- lesson4/js/myjavascript.js
+++ lesson4/js/myjavascript.js
@@ -2,4 +2,4 @@ function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("hide");
 }
 document.querySelector('#copyrightyear').textContent = new Date().getFullYear();
-document.getElementById('lastupdated').textContent = `Last Updated: ${document.lastModified}`;
+document.querySelector('#lastupdated').textContent = `Last Updated: ${document.lastModified}`;
