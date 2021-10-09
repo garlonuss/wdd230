7160e83eb2095eb95a723b6a07bca8ae7a8a9aa5
garlonuss <garlonuss@hotmail.com>
2021-10-09 15:40:03 -0600

    update

--- lesson4/js/myjavascript.js
+++ lesson4/js/myjavascript.js
@@ -1,6 +1,5 @@
 function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("hide");
 }
-toDate = dateFormat(new Date(), 'pretty-a');
 document.querySelector('#copyrightyear').textContent = new Date().getFullYear();
-document.querySelector('#lastupdated').textContent = `Last Updated: ${document.lastModified}<br>[Today: ${toDate}]`;
+document.querySelector('#lastupdated').textContent = `Last Updated: ${document.lastModified}<br>[Today: ${dateFormat(new Date(), 'pretty-a')}]`;
