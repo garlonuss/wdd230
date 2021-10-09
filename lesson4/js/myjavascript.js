4cde78f820bf6da33f5d4d7824b7f399fb521484
garlonuss <garlonuss@hotmail.com>
2021-10-09 15:52:39 -0600

    update

--- lesson4/js/myjavascript.js
+++ lesson4/js/myjavascript.js
@@ -3,3 +3,4 @@ function toggleMenu() {
 }
 document.querySelector('#copyrightyear').textContent = new Date().getFullYear();
 document.querySelector('#lastupdated').textContent = `Last Updated: ${document.lastModified}`;
+document.querySelector('#todaysdate').textContent = `[Today: ${dateFormat(new Date(), 'pretty-a')}]`;
