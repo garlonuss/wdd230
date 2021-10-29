a8ed4cb3c742313171a08570789ececde0a68ca7
garlonuss <garlonuss@hotmail.com>
2021-10-28 19:38:45 -0600

    update

--- lesson7/js/myjavascript.js
+++ lesson7/js/myjavascript.js
@@ -1,4 +1,6 @@
-
+/*/
+This section is for working with the gallery and lazy loading of items in the museum section
+/*/
 let imagesToLoad = document.querySelectorAll('img[data-src]');
 const imgOptions = {
   threshold: 1,
@@ -28,9 +30,16 @@ if ('IntersectionObserver' in window) {
   });
 }
 
+/*/
+This section is for hiding/showing specific part of the navigation
+/*/
 function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("hide");
 }
+
+/*/
+This section is for setting the date of the last page update at the bottom of the page
+/*/
 var today = new Date();
 document.querySelector('#copyrightyear').textContent = today.getFullYear();
 document.querySelector('#lastupdated').textContent = `Last Updated: ${document.lastModified}`;
@@ -61,9 +70,69 @@ if (today.getDay() == 5) {
   document.querySelector('.banner').setAttribute("style", "display: block;");
 }
 
+/*/
+This section is for calculating the number of days since the user has visited the page
+/*/
+var todaysDate = `${today.getMonth()}/${today.getDate()}/${today.getFullYear()}`
+if (!localStorage.getItem('lastVisit')) {
+  populateStorage(todaysDate);
+} else {
+  setVisit(todaysDate);
+}
+
+function populateStorage(todaysDate) {
+  localStorage.setItem('lastVisit', todaysDate);
+  /*/
+  alert("last visit: " + todaysDate)
+  /*/
+  localStorage.setItem('firstVisit', todaysDate);
+  /*/
+  alert("first visit: " + todaysDate)
+  /*/
+
+  setVisit(todaysDate);
+}
+function parseDateString(dateString) {
+  var mdy = dateString.split('/');
+  return new Date(mdy[2], mdy[0], mdy[1]);
+}
+function datediff(first, second) {
+  firstDate = parseDateString(first);
+  secondDate = parseDateString(second);
+  return Math.round((secondDate - firstDate) / (1000 * 60 * 60 * 24));
+}
+
+function setVisit(todaysDate) {
+  var lastTimeHere = localStorage.getItem('lastVisit');
+  var firstTimeHere = localStorage.getItem('firstVisit');
+  /*/
+  alert(lastTimeHere);
+  alert(todaysDate);
+  alert(firstTimeHere);
+  /*/
+  var span = datediff(lastTimeHere, todaysDate);
+  /*/
+  alert(span);
+  /*/
+  if (lastTimeHere == todaysDate) {
+    if (firstTimeHere == lastTimeHere) {
+      document.getElementById('lastTimeHere').textContent = `You haven't visited this page before today.`;
+    } else {
+      document.getElementById('lastTimeHere').textContent = `Your last visit here was earlier today.`;
+    }
+  } else {
+    document.getElementById('lastTimeHere').textContent = `Your last time viewing this page was ${span} days ago.`;
+  }
+  localStorage.setItem('lastVisit', todaysDate);
+}
+
+/*/
+This section is for populating the 5 day forecast
+/*/
 var dayvalue = today.getDay();
 document.querySelector('#day1').textContent = `${weekday[dayvalue]}`;
 document.querySelector('#day2').textContent = `${weekday[((dayvalue + 1) % 7)]}`;
 document.querySelector('#day3').textContent = `${weekday[((dayvalue + 2) % 7)]}`;
 document.querySelector('#day4').textContent = `${weekday[((dayvalue + 3) % 7)]}`;
 document.querySelector('#day5').textContent = `${weekday[((dayvalue + 4) % 7)]}`;
+
